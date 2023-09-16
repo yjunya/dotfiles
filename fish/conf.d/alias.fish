837210d11a0300ed91fb0340f2f87a6cf01695c9
yjunya <jun.officialuse@gmail.com>
2023-09-16 21:38:48 +0900

    ✨  add alias

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -5,6 +5,7 @@ alias ll="exa -ahl --icons --git"
 alias la="exa -ahl --icons --git"
 alias tree="exa --tree --icons --git-ignore"
 alias rm="trash"
+alias ip="ipconfig getifaddr en0"
 function mkcd
 	mkdir $argv
 	cd $argv
@@ -21,6 +22,10 @@ alias pp="cd (pbpaste)"
 alias c="clear"
 alias bk="cd .."
 alias main="cd ~/main"
-alias dotfiles="cd ~/main/dotfiles"
+alias dot="cd ~/main/dotfiles"
+alias capture="cd ~/ScreenCapture/"
 alias desk="cd ~/Desktop"
 alias down="cd ~/Downloads"
+
+# dena
+alias pococha="cd ~/main/works/01_pococha/01_code/"
