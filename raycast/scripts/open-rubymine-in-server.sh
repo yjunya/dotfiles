d9fc5a1c51db1a7087d7f68ab6fc92fa7d0c82ee
yjunya <jun.officialuse@gmail.com>
2023-09-16 22:25:00 +0900

    ✨ add raycast scripts

---
+++ raycast/scripts/open-rubymine-in-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open rubymine in server
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+source .env
+
+cd $WORKSPACE_PATH
+
+eval "$(rtx hook-env -s bash)"
+
+open /Applications/RubyMine.app
+
---
+++ raycast/scripts/restart-vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title restart-vpn
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📶
+
+source .env
+
+scutil --nc stop $VPN_NAME
+
+count=0
+while [ $count -lt 60 ]; do
+  if [[ "$(scutil --nc status $VPN_NAME | head -n 1)" == "Disconnected" ]]; then
+    scutil --nc start $VPN_NAME --user $USER --password $PASSWORD --secret $VPN_SECRET
+    break
+  fi
+  sleep 1
+done
+
