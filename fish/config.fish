f9a78cdd6af26286cc4bde3ce0e68e6d6a461418
yjunya <jun.officialuse@gmail.com>
2023-09-16 21:54:33 +0900

    ✨ update Brewfile

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 	eval (/opt/homebrew/bin/brew shellenv)
   starship init fish | source
+  rtx activate fish | source
+  rtx hook-env -s fish | source
 
   set -x GOPATH $HOME/go
   set -x PATH $PATH $GOPATH/bin
