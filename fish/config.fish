7c985551d224e0becd8de8c8083dff0935c17cf8
yjunya <jun.officialuse@gmail.com>
2024-08-19 09:29:47 +0900

    add encoding

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
+  set -x LANG en_US.UTF-8
+  set -x LC_ALL en_US.UTF-8
 	eval (/opt/homebrew/bin/brew shellenv)
   starship init fish | source
   mise activate fish | source
