00f7f00b74c5ab63370a45edd4516d025f417b57
yjunya <jun.officialuse@gmail.com>
2025-03-16 01:09:31 +0900

    Use init.lua

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -3,5 +3,6 @@ SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
 
 mkdir -p ~/.config/nvim
-ln -siv $(pwd)/init.vim ~/.config/nvim/init.vim
+# ln -siv $(pwd)/init.vim ~/.config/nvim/init.vim
+ln -siv $(pwd)/init.lua ~/.config/nvim/init.lua
 
