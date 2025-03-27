740ca3fcf7a6f84cb3fd30fd25a4b58d4650e845
yjunya <jun.officialuse@gmail.com>
2025-03-27 09:34:25 +0900

    Add ChatGPT hotkey

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,14 @@
-local open_wezterm = function()
-    local appName = "Ghostty"
-    local app = hs.application.get(appName)
+local open_app_func = function(appName)
+    return function()
+        local app = hs.application.get(appName)
 
-    if app == nil or app:isHidden() or not(app:isFrontmost()) then
-        hs.application.launchOrFocus(appName)
-    else
-        app:hide()
+        if app == nil or app:isHidden() or not(app:isFrontmost()) then
+            hs.application.launchOrFocus(appName)
+        else
+            app:hide()
+        end
     end
 end
 
-hs.hotkey.bind({"cmd"}, "/", open_wezterm)
+hs.hotkey.bind({"cmd"}, "/", open_app_func("Ghostty"))
+hs.hotkey.bind({"cmd", "shift"}, "/", open_app_func("ChatGPT"))
